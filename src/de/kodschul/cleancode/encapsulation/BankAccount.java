3009c6cdc2b9eb4e240b9184cdc07de1a74dd947
Erik Hoelzel <erik.hoelzel@posteo.de>
2024-10-14 21:51:08 +0100

    exercise encapsulation

---
+++ src/de/kodschul/cleancode/encapsulation/BankAccount.java
@@ -0,0 +1,30 @@
+package de.kodschul.cleancode.encapsulation;
+
+/**
+ * Enhance encapsulation of this class
+ */
+class BankAccount {
+    public String accountHolder;
+    public double balance;
+
+    public BankAccount(String accountHolder, double initialBalance) {
+        this.accountHolder = accountHolder;
+        this.balance = initialBalance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount; // Directly modifying the balance
+    }
+
+    public void withdraw(double amount) {
+        if (balance >= amount) {
+            balance -= amount; // Directly modifying the balance
+        } else {
+            System.out.println("Insufficient funds!");
+        }
+    }
+
+    public void printAccountDetails() {
+        System.out.println("Account Holder: " + accountHolder + ", Balance: " + balance);
+    }
+}
