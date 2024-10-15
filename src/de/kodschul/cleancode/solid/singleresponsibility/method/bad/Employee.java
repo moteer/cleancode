2fbf119ef77f757a619259eff37a0516298bd67c
Erik Hoelzel <erik.hoelzel@posteo.de>
2024-10-15 09:01:45 +0100

    Employee

---
+++ src/de/kodschul/cleancode/solid/singleresponsibility/method/bad/Employee.java
@@ -0,0 +1,14 @@
+package de.kodschul.cleancode.solid.singleresponsibility.method.bad;
+
+public class Employee {
+    private String name;
+    private String salary;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSalary() {
+        return salary;
+    }
+}
